6674335331c6e4a57b419f6970528678190e3bfd
Muhdksm <muhdkasim50@gmail.com>
2025-10-24 19:24:02 +0100

    new

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-const hamburger =
-document.getElementById('hamburger');
-const navLinks =
-document.getElementById('navLinks');
-const overlay =
-document.getElementById('overlay');
-const closeBtn =
-document.getElementById("closeBtn")
-
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+const overlay = document.getElementById('overlay');
+const closeBtn = document.getElementById('closeBtn');
+const closeHome = document.getElementById('closeHome');
+const closePjt = document.getElementById('closePjt');
+const closeCtn = document.getElementById('closeCtn');
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.add("active");
   hamburger.classList.add("active");
-  overlay.classList.toggle('active')
+  overlay.classList.add("active");
 });
 
-function closeMenu () {
-navLinks.classList.remove("active");
-hamburger.classList.remove("active");
-overlay.classList.remove("active")
-};
+function closeMenu() {
+  navLinks.classList.remove("active");
+  hamburger.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
 closeBtn.addEventListener("click", closeMenu);
 overlay.addEventListener("click", closeMenu);
+closeHome.addEventListener("click", closeMenu);
+closePjt.addEventListener("click", closeMenu);
+closeCtn.addEventListener("click", closeMenu);
+navLinks.addEventListener("click", closeMenu)
+
 
 
  
